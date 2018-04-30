34c1ec5aed2005a7687b8bb01113ca35fd3165ea
Hardik <hardikdg.sa@gmail.com>
2018-04-30 08:09:12 +0530

    iOS platform added

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
+const nav = document.querySelector('ion-nav');
+
 @Component({
   selector: 'app-page-home',
   templateUrl: 'home.page.html',
